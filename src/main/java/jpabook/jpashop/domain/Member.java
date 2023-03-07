ec07d0bc9770dac5a6888b850cdb3ebb804e596f
dam33534@naver.com <dam33534@naver.com>
2023-03-07 15:19:57 +0900

    feat : entity 매핑 완료
    feat : entity 매핑 완료

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -3,13 +3,18 @@ package jpabook.jpashop.domain;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Member {
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
     private Long id;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 
     private String name;
     private String city;
